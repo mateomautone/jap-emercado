16f65521423724b8b799a15f6c3efc38556bb40c
MartLopezF <23martinlopez@gmail.com>
2023-08-17 21:28:23 -0300

    products page

---
+++ js/products.js
@@ -0,0 +1,36 @@
+async function showProduct() {
+  const url = 'producto.json';
+
+  try {
+      const response = await fetch(url);
+      const datos = await response.json();
+
+      let htmlContentToAppend = "";
+      for (let i = 0; i < datos.products.length; i++) {
+          let product = datos.products[i];
+
+          htmlContentToAppend +=`
+          <a href="products.html" class="listado-item listado-item-action">
+              <div class="row">
+                  <div class="col-md-3">
+                      <img src="${product.image}" alt="${product.description}" class="img-thumbnail"></img>
+                  </div> 
+                  <div class="col-md-5">
+                       <h2>${product.name}</h2>
+                       <small>${product.soldCount} artículos</small>
+                      <p>${product.description}</p>
+                      <h3>${product.currency} ${product.cost}</h3>
+                  </div>
+              </div>
+          </a>
+         ` ;
+      }
+      document.getElementById("prod-list").innerHTML = htmlContentToAppend;
+  } catch (error) {
+      console.error('Error:', error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  showProduct(); // Invoca la funcion
+});
