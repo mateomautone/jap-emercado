0bc68f9d8c1110be759193521e4bfe2a5754d775
AlejandroSacco <alejandroivan2001@gmail.com>
2023-10-13 15:00:02 -0300

    Precios correctos

--- js/cart.js
+++ js/cart.js
@@ -19,17 +19,17 @@ function showShop(array){
         <td id="unitSub">USD ${array.unitCost}</td>
       </tr>
     `;
-    carrito.forEach(element => {
+    for(let j = 0; j < carrito.length; j++){
       htmlAppend += `
       <tr>
-        <th scope="row" style="width: 15%;"><img style="width: 45%;" src="${element.image}"</th>
-        <td>${element.name}</td>
-        <td id="unitCost">USD ${element.price}</td>
-        <td style="width: 25%;"> <input id="unitCant" style="width: 25%;" type="text" value="1"> </td>
-        <td id="unitSub">USD ${element.price}</td>
+        <th scope="row" style="width: 15%;"><img style="width: 45%;" src="${carrito[j].image}"</th>
+        <td>${carrito[j].name}</td>
+        <td id="unitCost${j}">USD ${carrito[j].price}</td>
+        <td style="width: 25%;"> <input id="unitCant${j}" style="width: 25%;" type="number" value="1"> </td>
+        <td id="unitSub${j}">USD ${carrito[j].price}</td>
       </tr>
     `;
-    });
+    };
     document.getElementById("tablaProd").innerHTML = htmlAppend;
 };
 
@@ -38,18 +38,24 @@ document.addEventListener("DOMContentLoaded", ()=>{
         if (resultObj.status === "ok"){
           cartInfo = resultObj.data.articles;
           showShop(cartInfo[0]);
+        for(let j = 0; j < carrito.length; j++){
+          document.getElementById("unitCant"+j).addEventListener("input",()=>{
+            document.getElementById("unitSub"+j).innerHTML = "USD " + parseInt(document.getElementById("unitCant"+j).value)*(parseInt(carrito[j].price));
+          }); 
+        }
         }
         document.getElementById("unitCant").addEventListener("input", ()=>{
           let cantidad = parseFloat(document.getElementById("unitCant").value);
           let costoUnitario = parseFloat(cartInfo[0].unitCost);
           let subtotal = (cantidad*costoUnitario);
-            document.getElementById("unitSub").innerHTML = `USD ${subtotal.toFixed(2)}`;
+            document.getElementById("unitSub").innerHTML = `USD ${subtotal}`;
         });
-      });
-
+    });
     document.getElementById("empty").addEventListener("click", ()=>{
       localStorage.removeItem("cart");
       location.reload();
-    })
+    });
+    
+    console.log(carrito);
   });
 
