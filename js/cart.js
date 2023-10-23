0c6767559e9f8975d4c551c1d347245344c29e09
AlejandroSacco <alejandroivan2001@gmail.com>
2023-10-23 17:36:29 -0300

    mas cambios

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,6 @@
 const cart_url = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 let cartInfo = [];
 let carrito = JSON.parse(localStorage.getItem('cart')) || [];
-var Sub = 10;
-let shipp = 0;
 
 function showShop(){
     let htmlAppend = "";
@@ -14,7 +12,6 @@ function showShop(){
         	<td id="unitCost${j}">USD ${carrito[j].price}</td>
         	<td style="width: 25%;"> <input id="unitCant${j}" style="width: 25%;" type="number" value="1" min="0"> </td>
         	<td id="unitSub${j}">USD ${carrito[j].price}</td>
-			<td> <i id="delete${j}" class="fa-solid fa-trash fa-2xs" style="color: #d80106;"></i> </td> 
       	</tr>
     	`;
     };
@@ -36,83 +33,35 @@ document.addEventListener("DOMContentLoaded", ()=>{
 			}
         };
     });
+
 	showShop();
 
-	for(let j = 0; j < carrito.length; j++){
+    for(let j = 0; j < carrito.length; j++){
         document.getElementById("unitCant"+j).addEventListener("input",()=>{
-            if(document.getElementById("unitCant"+j).value === ""){
-                document.getElementById("unitSub"+j).innerHTML = "USD 0";
+        	if(document.getElementById("unitCant"+j).value === ""){
+            	document.getElementById("unitSub"+j).innerHTML = "USD 0";
             }
             else{
                 document.getElementById("unitSub"+j).innerHTML = "USD " + parseInt(document.getElementById("unitCant"+j).value)*(parseInt(carrito[j].price));
             }
-        });
-    };
-
-	for(let a = 0; a < carrito.length; a++){
-		document.getElementById("unitCant"+a).addEventListener("input",()=>{
-			Sub = (parseInt(document.getElementById("unitSub" + a).innerHTML.slice(3)));
-			document.getElementById("subtotal").innerHTML = "USD " + Sub;
-			
-		});
-	};
-
-	for(let i = 0; i < carrito.length; i++){
-		document.getElementById("delete"+i).addEventListener("click", ()=>{
-			let j = 0;
-			while(j < carrito.length && carrito[j].name != document.getElementById("name"+i).innerHTML){
-				j++;
-			}
-			carrito.splice(j, 1);
-			localStorage.setItem('cart', JSON.stringify(carrito));
-			location.reload();
-		});
-	};
-
-	document.getElementById("premiumradio").addEventListener("input", ()=>{
-		if(document.getElementById("premiumradio").checked){
-			shipp = (0.15*Sub);
-		}
-		document.getElementById("envio").innerHTML = "USD " + shipp;
-	});
-	document.getElementById("expressradio").addEventListener("input", ()=>{
-		if(document.getElementById("expressradio").checked){
-			shipp = (0.07*Sub);
-		}
-		document.getElementById("envio").innerHTML = "USD " + shipp;
-	});
-	document.getElementById("standardradio").addEventListener("input", ()=>{
-		if(document.getElementById("standardradio").checked){
-			shipp = (0.05*Sub);
-		}
-		document.getElementById("envio").innerHTML = "USD " + shipp;
-	});
-
-	document.getElementById("creditcard").addEventListener("input", ()=>{
-		document.getElementById("numberbank").disabled = true;
-		document.getElementById("numbercard").disabled = false;
-		document.getElementById("codecard").disabled = false;
-		document.getElementById("datecard").disabled = false;
-	});
-
-	document.getElementById("bank").addEventListener("input", ()=>{
-		document.getElementById("numberbank").disabled = false;
-		document.getElementById("numbercard").disabled = true;
-		document.getElementById("codecard").disabled = true;
-		document.getElementById("datecard").disabled = true;
-	});
-
-
-	document.getElementById("finish").addEventListener("click", ()=>{
-		if((document.getElementById("street").value != "" && document.getElementById("number").value != "" && document.getElementById("corner").value != "") &&
-		(document.getElementById("premiumradio").checked || document.getElementById("expressradio").checked || document.getElementById("standardradio").checked) &&
-		(document.getElementById("creditcard").checked || document.getElementById("bank").checked)){
-			document.getElementById("successmsg").hidden = false;
-		}
-		else{
-			document.getElementById("failmsg").hidden = false;
-		}
-	});	
+        }); 
+    }
+        
+    document.getElementById("unitCant").addEventListener("input", ()=>{
+        let cantidad = parseFloat(document.getElementById("unitCant").value);
+        let costoUnitario = parseFloat(cartInfo[0].unitCost);
+        let subtotal = (cantidad*costoUnitario);
+        if(document.getElementById("unitCant").value === ""){
+            document.getElementById("unitSub").innerHTML = `USD 0`;
+        }
+        else{
+            document.getElementById("unitSub").innerHTML = `USD ${subtotal}`;
+        }
+    });
+    
+    document.getElementById("empty").addEventListener("click", ()=>{
+      	localStorage.removeItem("cart");
+      	location.reload();
+    });
 });
-
 
