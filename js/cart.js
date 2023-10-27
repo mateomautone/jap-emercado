c5f3b696692be61dfa72b5d2b589ab6ec4fa1cd0
AlejandroSacco <alejandroivan2001@gmail.com>
2023-10-26 22:04:15 -0300

    Form con bootstrap

--- js/cart.js
+++ js/cart.js
@@ -4,56 +4,7 @@ let carrito = JSON.parse(localStorage.getItem('cart')) || [];
 let Sub = 0;
 let subFinal = 0;
 
-/* VALIDAR Form */
 
-function validateForm() {
-	let premiumChecked = document.getElementById('premiumradio').checked;
-	let expressChecked = document.getElementById('expressradio').checked;
-	let standardChecked = document.getElementById('standardradio').checked;
-	let streetValue = document.getElementById('street').value;
-	let numberValue = document.getElementById('number').value;
-	let cornerValue = document.getElementById('corner').value;
-
-	let isValid = true;
-
-	if (!premiumChecked && !expressChecked && !standardChecked) {
-		isValid = false;
-		document.getElementById('radioInvalid').style.display = 'block';
-	} else {
-		document.getElementById('radioInvalid').style.display = 'none';
-	}
-
-	if (streetValue === "") {
-		isValid = false;
-		document.getElementById('streetInvalid').style.display = 'block';
-	} else {
-		document.getElementById('streetInvalid').style.display = 'none';
-	}
-
-	if (numberValue === "") {
-		isValid = false;
-		document.getElementById('numberInvalid').style.display = 'block';
-	} else {
-		document.getElementById('numberInvalid').style.display = 'none';
-	}
-
-	if (cornerValue === "") {
-		isValid = false;
-		document.getElementById('cornerInvalid').style.display = 'block';
-	} else {
-		document.getElementById('cornerInvalid').style.display = 'none';
-	}
-
-	if (isValid) {
-		let popup = document.getElementById("successPopup");
-        popup.style.display = "block";
-        setTimeout(function(){
-            popup.style.display = "none";
-        }, 2000);
-    }
-	}
-
-// FIN VALIDAR FORM
 
 function showShop(){
     let htmlAppend = "";
@@ -62,9 +13,9 @@ function showShop(){
       	<tr>
         	<th scope="row" style="width: 15%;"><img style="width: 45%;" src="${carrito[j].image}"</th>
         	<td id="name${j}">${carrito[j].name}</td>
-        	<td id="unitCost${j}">USD ${carrito[j].price}</td>
+        	<td id="unitCost${j}">${carrito[j].currency} ${carrito[j].price}</td>
         	<td style="width: 25%;"> <input id="unitCant${j}" style="width: 25%;" type="number" value="1" min="0"> </td>
-        	<td id="unitSub${j}">USD ${carrito[j].price}</td>
+        	<td id="unitSub${j}">${carrito[j].currency} ${carrito[j].price}</td>
             <td> <i id="delete${j}" class="fa fa-trash" style="color: #f00006;"></i> </td>
       	</tr>
     	`;
@@ -108,7 +59,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 			}
 			subFinal += Sub;
 		}
-		document.getElementById("subtotal").innerHTML = "USD " + subFinal;
+		document.getElementById("subtotal").innerHTML = "USD " + subFinal.toFixed(2);
 		subFinal = 0;
 	};
 	mostrarSub();
@@ -120,31 +71,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 	//mostrar costo de envío
 	document.getElementById("premiumradio").addEventListener("click", ()=>{
-		document.getElementById("envio").innerHTML = "USD " + parseFloat(document.getElementById("subtotal").innerHTML.slice(4))*0.15;
+		document.getElementById("envio").innerHTML = "USD " + (parseFloat(document.getElementById("subtotal").innerHTML.slice(4))*0.15).toFixed(2);
 	});
 	document.getElementById("expressradio").addEventListener("click", ()=>{
-		document.getElementById("envio").innerHTML = "USD " + parseFloat(document.getElementById("subtotal").innerHTML.slice(4))*0.07;
+		document.getElementById("envio").innerHTML = "USD " + (parseFloat(document.getElementById("subtotal").innerHTML.slice(4))*0.07).toFixed(2);
 	});
 	document.getElementById("standardradio").addEventListener("click", ()=>{
-		document.getElementById("envio").innerHTML = "USD " + parseFloat(document.getElementById("subtotal").innerHTML.slice(4))*0.05;
+		document.getElementById("envio").innerHTML = "USD " + (parseFloat(document.getElementById("subtotal").innerHTML.slice(4))*0.05).toFixed(2);
 	});
 
 	//mostrar total
 	setInterval(()=>{
-		document.getElementById("total").innerHTML = "USD " + (parseFloat(document.getElementById("envio").innerHTML.slice(4)) + parseFloat(document.getElementById("subtotal").innerHTML.slice(4)));
+		document.getElementById("total").innerHTML = "USD " + ((parseFloat(document.getElementById("envio").innerHTML.slice(4)) + parseFloat(document.getElementById("subtotal").innerHTML.slice(4))).toFixed(2));
 	}, 10);
 
-    for(let j = 0; j < carrito.length; j++){
-        document.getElementById("unitCant"+j).addEventListener("input",()=>{
-        	if(document.getElementById("unitCant"+j).value === ""){
-            	document.getElementById("unitSub"+j).innerHTML = "USD 0";
-            }
-            else{
-                document.getElementById("unitSub"+j).innerHTML = "USD " + parseInt(document.getElementById("unitCant"+j).value)*(parseInt(carrito[j].price));
-            }
-        }); 
-    };
-    
+	//Borrar elementos
     for(let i = 0; i < carrito.length; i++){
 		document.getElementById("delete"+i).addEventListener("click", ()=>{
 			let j = 0;
@@ -156,6 +97,28 @@ document.addEventListener("DOMContentLoaded", ()=>{
 			location.reload();
 		});
 	};
+	
+	/* VALIDAR Form */
+	'use strict'
+      
+    const form = document.getElementById("form")
+      
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+		else{
+			event.preventDefault();
+			document.getElementById("successPopup").style.display = "block";
+			setTimeout(function(){
+				document.getElementById("successPopup").style.display = "none";
+			}, 2000);
+		}
+      
+        form.classList.add('was-validated')
 
+    }, false);
+	// FIN VALIDAR FORM
 });
 
