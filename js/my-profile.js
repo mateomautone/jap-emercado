635122a2905b18c2d9135f7ef975bc20273ab360
AlejandroSacco <alejandroivan2001@gmail.com>
2023-11-10 13:40:22 -0300

    Form

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,47 +1,3 @@
-//Llenar campo de mail con lo de el login
-
-let email = document.getElementById("profemail")
-let localemail = localStorage.getItem("name")
-
-email.value = localemail
-
-//Guardo elementos necesarios en variables
-document.addEventListener("DOMContentLoaded", ()=> {
-let file = document.getElementById("subir-img");
-let defaultImg = "./img/avatar.png";
-let img = document.getElementById("img-perfil")
-
-//Guardo en localStorage la variable img
-let savedImg = localStorage.getItem("img");
-if (savedImg) {
-    img.src = savedImg; 
-} else {
-    img.src = defaultImg;
-}
-//Cambia foto de perfil
-file.addEventListener("change", e=>{
-    if (e.target.files[0]){
-        let reader = new FileReader();
-        reader.onload = function (e){
-        img.src = e.target.result;
-        localStorage.setItem("img", e.target.result);
-    }
-    reader.readAsDataURL(e.target.files[0])
-    
-    }else{
-        img.src = defaultImg;
-    }
-});
-//Evento para guardar los cambios de la imagen
-    document.getElementById("guardar-cambios").addEventListener("click", ()=>{
-        let savedImg = localStorage.getItem("img");
-        if (savedImg) {
-            img.src = savedImg;
-        }
-    });
-});
-
-
 function savePersonalInfo() {
     let personalInfo = {};
     personalInfo.profnombre = document.getElementById("profnombre").value;
@@ -52,7 +8,7 @@ function savePersonalInfo() {
     personalInfo.profnumero = document.getElementById("profnumero").value;
 
     localStorage.setItem("profile", JSON.stringify(personalInfo));
-}
+};
 
 function getSavedPersonalInfo() {
     let personalInfo = JSON.parse(localStorage.getItem("profile"));
@@ -64,5 +20,62 @@ function getSavedPersonalInfo() {
         document.getElementById("profemail").value = personalInfo.profemail;
         document.getElementById("profnumero").value = personalInfo.profnumero;
     };
-}
-getSavedPersonalInfo();
+};
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    getSavedPersonalInfo();
+    //Llenar campo de mail con lo de el login
+    document.getElementById("profemail").value = localStorage.getItem("name");
+
+    //Guardo elementos necesarios en variables
+    let file = document.getElementById("subir-img");
+    let defaultImg = "./img/avatar.png";
+    let img = document.getElementById("img-perfil")
+
+    //Guardo en localStorage la variable img
+    let savedImg = localStorage.getItem("img");
+    if (savedImg) {
+        img.src = savedImg; 
+    } else {
+        img.src = defaultImg;
+    }
+
+    'use strict'
+      
+    const form = document.getElementById("form")
+      
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        else{
+            savePersonalInfo();
+        }
+      
+        form.classList.add('was-validated')
+
+    }, false);
+
+    //Cambia foto de perfil
+    file.addEventListener("change", e=>{
+        if (e.target.files[0]){
+            let reader = new FileReader();
+            reader.onload = function (e){
+                img.src = e.target.result;
+                localStorage.setItem("img", e.target.result);
+            }
+            reader.readAsDataURL(e.target.files[0])
+        }else{
+            img.src = defaultImg;
+        }
+    });
+
+    //Evento para guardar los cambios de la imagen
+    document.getElementById("guardar-cambios").addEventListener("click", ()=>{
+        let savedImg = localStorage.getItem("img");
+        if (savedImg) {
+            img.src = savedImg;
+        }
+    });
+});
