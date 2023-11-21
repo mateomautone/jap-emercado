ab73df211b06f8458da2d5c7c51d39b131216c03
Manuel-Ventura-Work <manuelventurawork@gmail.com>
2023-11-21 14:50:16 -0300

    limpieza 1

--- js/login.js
+++ js/login.js
@@ -1,29 +1,9 @@
-/* function check(){
-   let nombre = document.getElementById ("name").value;
-    if(document.getElementById("name").value !== "" && document.getElementById("pass").value !== ""){
-        localStorage.setItem ("name", nombre);
-        location.replace("index.html");
-    }
-    else{
-        alert('Faltan datos');
-    }
-}
- */
-
-/* document.getElementById("boton").addEventListener('click', () =>{
-        check();
-});
- */
-
-
-
 document.getElementById('boton').addEventListener('click', function (event) {
     event.preventDefault();
 
     const username = document.getElementById('name').value;
     const password = document.getElementById('pass').value;
-    let nombre = document.getElementById ("name").value;
-    localStorage.setItem ("name", nombre);
+    localStorage.setItem ("name", username);
     fetch('http://localhost:3000/login', {
       method: 'POST',
       headers: {
@@ -33,12 +13,10 @@ document.getElementById('boton').addEventListener('click', function (event) {
     })
     .then(response => response.json())
     .then(data => {
-      // Store the received token in local storage
       localStorage.setItem('token', data.token);
-
-      // Redirect to the main website
       location.replace("index.html");
     })
+    
     .catch(error => {
       console.error('Login failed:', error);
     });
